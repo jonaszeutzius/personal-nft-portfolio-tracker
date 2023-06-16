516d81207d7e5f79f9f605ce7ecfa651a81b769c
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-16 11:54:29 -0600

    nfts displayed in table

--- src/NFTPortfolioTracker.js
+++ src/NFTPortfolioTracker.js
@@ -4,7 +4,8 @@ import './NFTPortfolioTracker.css';
 import './App.css'
 
 const NFTPortfolioTracker = () => {
-    const [nfts, setNfts] = useState([]);
+    const [blockchain, setBlockchain] = useState('eth-main');
+    const [nfts, setNfts] = useState(null);
     const [address, setAddress] = useState('');
     // Additional state variables if needed
   
@@ -12,7 +13,7 @@ const NFTPortfolioTracker = () => {
     const fetchNFTs = () => {
         const options = {
           method: 'GET',
-          url: `http://localhost:8080/v1/nfts/owner/${address}?chain=eth-main&include_nft_details=true&cursor=7&page_size=20`,
+          url: `http://localhost:8080/v1/nfts/owner/${address}?chain=${blockchain}&include_nft_details=true&cursor=7&page_size=20`,
           headers: { accept: 'application/json', 'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW' },
         };
         
@@ -29,21 +30,50 @@ const NFTPortfolioTracker = () => {
       useEffect(() => {
         fetchNFTs();
       }, []);
+      const handleBlockchainChange = (event) => {
+        setBlockchain(event.target.value);
+      };
       return (
         <div>
           <h1 className='title'>NFT Portfolio Tracker</h1>
-          <p>Input a wallet address below to see what NFTs that wallet owns.</p>
+          <p className='message'>Select a chain and input a wallet address below to see what NFTs that wallet owns.</p>
           <div className='inputContainer'>
+            <select name='blockchain' value={blockchain} onChange={handleBlockchainChange}>
+              <option value="eth-main">eth-main</option>
+              <option value="arbitrum-main">arbitrum-main</option>
+              <option value="optimism-main">optimism-main</option>
+              <option value="poly-main">poly-main</option>
+              <option value="bsc-main">bsc-main</option>
+              <option value="eth-goerli">eth-goerli</option>
+            </select>
             <input type="text" placeholder="Wallet Address" onChange={(e) => setAddress(e.target.value)} />
             <button onClick={fetchNFTs}>View Portfolio</button>
           </div>
-          {nfts.map((nft) => (
-            <div key={nft.id}>
-              <h2>{nft.nft_details?.token_name || 'N/A'}</h2>
-              <img src={nft.nft_details.cached_images.tiny_100_100 || 'placeholder-image-url'} alt="NFT Image" />
-              {/* Render additional NFT information */}
-            </div>
-          ))}
+          {nfts && (
+            <table>
+              <thead>
+                  <tr style={{ backgroundColor: '#f2f2f2' }}>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Token Name</th>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Id</th>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Token Type</th>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Contract Address</th>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Image</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {nfts.map((nft, index) => (
+                    console.log(nft),
+                      <tr key={index} style={{ backgroundColor: index % 2 === 0 ? '#f2f2f2' : 'white' }}>
+                        <td style={{ padding: '10px', textAlign: 'left' }}>{nft.nft_details.token_name}</td>
+                        <td style={{ padding: '10px', textAlign: 'left' }}>{nft.id}</td>
+                        <td style={{ padding: '10px', textAlign: 'left' }}>{nft.token_type}</td>
+                        <td style={{ padding: '10px', textAlign: 'left' }}>{nft.contract_address}</td>
+                        <td style={{ padding: '10px', textAlign: 'left' }}><img src={nft.nft_details.cached_images.tiny_100_100}/></td>
+                      </tr>
+                  ))}
+              </tbody>
+            </table>
+          )}
         </div>
       );
   };
